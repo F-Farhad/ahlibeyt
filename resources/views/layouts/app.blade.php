b573b1177209de90e3e7d105fea64339a3ceb691
Farhod <FFataev@kpfu.ru>
2023-06-05 14:23:28 +0300

    added fonts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,10 +15,19 @@
         }
     </style> --}}
 
+    {{-- <link href="https://allfont.ru/allfont.css?fonts=ds-arabic" rel="stylesheet" type="text/css" />
+    <style>
+      blockquote {
+        font-family: 'DS Arabic', arial;
+        font-size: 48px;
+        text-shadow: 4px 4px 4px #aaa;
+      }
+    </style> --}}
+
     <!-- AlpineJS -->
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js" defer></script>
     <!-- Font Awesome -->
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" integrity="sha256-KzZiKy0DWYsnwMF+X1DvQngQ2/FxF7MF3Ff72XcpuPs=" crossorigin="anonymous"></script>
+    {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/js/all.min.js" integrity="sha256-KzZiKy0DWYsnwMF+X1DvQngQ2/FxF7MF3Ff72XcpuPs=" crossorigin="anonymous"></script> --}}
     @vite('resources/css/app.css')
     @livewireStyles
 </head>
