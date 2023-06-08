f2b0d0337aceea5d6959bfce005090790ed34a0d
Farhod <fataev_f@mail.ru>
2023-06-08 21:58:05 +0300

    added 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,15 @@
+<x-app-layouts meta-title="Ahlibeyt" meta-description="">
+
+
+<section class="flex items-center bg-gray-50 dark:bg-gray-700 mx-auto py-8">
+    <div class="container flex flex-col items-center ">
+        <div class="flex flex-col gap-6 max-w-md text-center">
+            <h2 class="font-extrabold text-9xl text-gray-600">
+                <span class="sr-only">Error</span>404
+            </h2>
+            <p class="text-2xl md:text-3xl dark:text-gray-400">Страница не найдена</p>
+            <a href="{{route('main')}}" class="px-8 py-4 text-xl font-semibold rounded bg-purple-600 text-gray-50 hover:text-gray-200">Вернуться на главную</a>
+        </div>
+    </div>
+</section>
+</x-app-layouts>
