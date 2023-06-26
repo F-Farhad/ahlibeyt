9d2d9ffc581650be6438e1e04e8a5dacc1c07a97
Farhod <FFataev@kpfu.ru>
2023-06-26 09:49:57 +0300

    added favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{$metaTitle ?: 'Ahlibeyt'}}</title>
-    <meta name="author" content="">
+
+    <link rel="apple-touch-icon" sizes="180x180" href="{{asset('favicon_io/apple-touch-icon.png')}}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{asset('favicon_io/favicon-32x32.png')}}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{asset('favicon_io/favicon-16x16.png')}}">
+
+    <meta name="author" content="Farhod Fataev">
     <meta name="description" content="{{$metaDescription}}">
 
     <!-- AlpineJS -->
